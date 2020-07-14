81d7d315ce20d0772fad8af349b9345c16d4b6d1
Ilya Emelyanov <emel.9616@gmail.com>
2020-07-14 14:44:11 +0700

    IHub-161892 Write a description of the work to create integration

--- start_integration.py
+++ start_integration.py
@@ -1,10 +1,19 @@
 from integration_log import IntegrationLog, LogLevel
 from integration import Integration
+from jsonschema import validate
 import json
 import traceback
 
-with open('settings.json', "rb") as SFile:
-    settings_data = json.loads(SFile.read().decode('utf-8'))
+with open('settings.json', "rb") as PFile:
+    settings_data = json.loads(PFile.read().decode('utf-8'))
+
+with open('settings_schema.json', "rb") as PFile:
+    data_schema = json.loads(PFile.read().decode('utf-8'))
+
+try:
+    validate(instance=settings_data, schema=data_schema)
+except Exception as e:
+    raise Exception("Incorrect value in the settings file\n{}".format(str(e)))
 
 ov_url = settings_data['ovUrl']
 ov_access_key = settings_data['ovAccessKey']
@@ -18,20 +27,7 @@ with open('ihub_parameters.json', "rb") as PFile:
 
 process_id = ihub_data['processId']
 
-integration_log = IntegrationLog(process_id, ov_url, ov_access_key, ov_secret_key,
-                                 ov_integration_name, ov_token=True)
-
+integration_log = IntegrationLog(process_id, ov_url, ov_access_key, ov_secret_key, ov_integration_name, ov_token=True)
 integration = Integration(integration_log)
 
-try:
-    integration.start()
-except Exception as e:
-    if type(e).__name__ == 'IntegrationError':
-        error_message = str(e.message)
-        description = str(e.description)
-    else:
-        error_message = repr(e)
-        description = traceback.format_exc()
-
-    integration_log.add(LogLevel.ERROR, error_message, description)
-    raise Exception(error_message)
+integration.start()
