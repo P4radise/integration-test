cc172869c08bb965bfb1a32bdd295639b1020aca
Ilya Emelyanov <emel.9616@gmail.com>
2022-03-23 23:39:49 +0200

    Update start_integration.py

--- start_integration.py
+++ start_integration.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 try:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.txt'], shell=True)
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--no-warn-script-location', '-r', 'python_dependencies.txt'])
 except Exception as e:
     raise Exception(f'Failed to install package. Exception [{str(e)}]')
 
