15d63f89b1d85b851b8775c6d1f1acd0f812cbbd
P4radise <emel.9616@gmail.com>
2022-04-14 14:26:10 +0700

    Test Popen with shell is True

--- start_integration.py
+++ start_integration.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-subprocess.Popen([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.txt'])
+subprocess.Popen([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.txt'], shell=True)
 
 
 from onevizion import IntegrationLog, LogLevel
